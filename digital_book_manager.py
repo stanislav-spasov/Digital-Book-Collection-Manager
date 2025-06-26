161a16faaf608b213424f2733694a50c5ef5d056
Stanislav Spasov <stanislavspasov91@gmail.com>
2025-06-26 13:25:45 +0300

    book manager update

--- digital_book_manager.py
+++ digital_book_manager.py
@@ -1,12 +1,14 @@
-from store import title, authors, statuses
-
+title = []
+authors = []
+statuses = []
 
 def add_book(add_title: str, add_author: str, title_list: list[str], author_list: list[str], status_list: list[str]):
-    if add_title not in title_list:
-        title_list.append(add_title)
-        author_list.append(add_author)
-        status_list.append("Unread")
+    if f"Title: {add_title}" not in title_list:
+        title_list.append(f"Title: {add_title}")
+        author_list.append(f"Author: {add_author}")
+        status_list.append("Status: Unread")
         print(f"\nThe book '{add_title}' from '{add_author}' has been added.")
+        save_fail()
     else:
         print(f"\nThe book '{add_title}' exist in your book manager.")
 
@@ -19,6 +21,33 @@ def mark_as_read(title_read: str, title_list: list[str], status_list: list[str])
     else:
         print(f"\nThe book '{title_read}' is not exist in your book manager.")
 
+
+def open_file():
+    f = open('storage.txt', 'r')
+    print(f.read())
+    f.close()
+
+
+def save_fail():
+    with open('storage.txt', 'w+') as f:
+        store = False
+        new_list = []
+        for x in range(len(title)):
+            if store:
+                break
+            for y in range(len(authors)):
+                if store:
+                    break
+                for z in range(len(statuses)):
+                    new_list.append(f"{title[x]} | {authors[y]} | {statuses[z]}")
+                    x += 1
+                    y += 1
+                    if z == len(statuses) - 1:
+                        store = True
+                        break
+        f.write("\n".join(new_list))
+        print("File written successfully")
+    f.close()
 
 def process_command(current_choice: str, titles: list[str], author: list[str], status: list[str]):
     if current_choice == "1":
@@ -34,19 +63,33 @@ def process_command(current_choice: str, titles: list[str], author: list[str], s
     elif current_choice == "4":
         ...
     elif current_choice == "5":
-        ...
+        open_file()
     elif current_choice == "6":
         ...
     elif current_choice == "7":
         ...
     elif current_choice == "8":
-        ...
+        exit()
     else:
         print("Invalid option. Please choose a number from 1 to 8.")
 
+def load_file_data():
+    with open("storage.txt", "r") as f:
+        books = []
+        for line in f:
+            books.append(line.strip().split(" | "))
+        for book_title in range(len(books)):
+            title.append(books[book_title][0])
+        for book_authors in range(len(books)):
+            authors.append(books[book_authors][1])
+        for book_status in range(len(books)):
+            statuses.append(books[book_status][2])
+        f.close()
 
 def main():
     print("Welcome to the Digital Book Collection Manager \n")
+
+    load_file_data()
 
     while True:
         print("\nPlease choose an option:")
@@ -59,10 +102,8 @@ def main():
         print("7. Delete a book")
         print("8. Exit")
 
-        choice = input("\nEnter your choice (1-8): ")
+        choice = input("\nEnter your choice (1-9): ")
 
         process_command(choice, title, authors, statuses)
-        print(title)
-
 
 main()
